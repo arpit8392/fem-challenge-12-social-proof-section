2d7ca57982a860b3d9a89d9e1990f9c01fff6207
Arpit Namdev <arpit8392@gmail.com>
2023-05-28 17:24:24 +0530

    Fix Vertical Spacing

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { reviews, testimonials } from '@/data'
 
 export default function Home() {
 	return (
-		<main className='relative flex flex-col space-y-12 px-6 py-20 md:px-40 md:py-32'>
+		<main className='relative flex flex-col space-y-12 px-6 py-20 md:px-40 md:py-28'>
 			<section className='flex flex-col space-y-10 md:flex-row md:items-center md:justify-between'>
 				<Header />
 				<Reviews reviews={reviews} />
